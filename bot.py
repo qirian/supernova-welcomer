cc6bbddfebfdf9d793531c8bbec41be7133f85ec
Levin Yilmaz <qirianprivate@hotmail.com>
2025-09-19 08:15:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # ------------------------
 WELCOME_CHANNEL_ID = 1418440616131428482
 LEAVE_CHANNEL_ID = 1418441039701610516
-AUTHOR_ICON = "https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
+
+AUTHOR_ICON = "https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024"
+THUMBNAIL = "https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024"
+WELCOME_IMAGE = "https://media.discordapp.net/attachments/1401822345953546284/1418437611193765888/Welcome.png?ex=68ce1e77&is=68ccccf7&hm=ea11922ec548a7438ce45a26eb96988ce281f7e9b57ebd240ffea5d3778f452e&="
+LEAVE_IMAGE = "https://media.discordapp.net/attachments/1401822345953546284/1418437610828988546/Good-Bye.png?ex=68ce1e77&is=68ccccf7&hm=bcf1f0ebb001ceeb793c4ed9b7022973d6d057b6ba34fcd171f3ca587b122499&="
 
 # ------------------------
 # Events
@@ -60,6 +64,8 @@ async def on_member_join(member):
             color=0x8f1eae
         )
         embed.set_author(name="Supernova x Welcomer", icon_url=AUTHOR_ICON)
+        embed.set_thumbnail(url=THUMBNAIL)
+        embed.set_image(url=WELCOME_IMAGE)
         embed.set_footer(text="© 2022–2024 Superbova. All Rights Reserved.", icon_url=AUTHOR_ICON)
         await channel.send(embed=embed)
 
@@ -73,6 +79,8 @@ async def on_member_remove(member):
             color=0x8f1eae
         )
         embed.set_author(name="Supernova x Welcomer", icon_url=AUTHOR_ICON)
+        embed.set_thumbnail(url=THUMBNAIL)
+        embed.set_image(url=LEAVE_IMAGE)
         embed.set_footer(text="© 2022–2024 Superbova. All Rights Reserved.", icon_url=AUTHOR_ICON)
         await channel.send(embed=embed)
 
