b0c72b219851981ecad8fd0bb661d6902d4cda7a
Levin Yilmaz <qirianprivate@hotmail.com>
2025-09-19 06:20:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,18 +38,20 @@ GOODBYE_CHANNEL_ID = 1418441039701610516
 # ------------------------
 # Utility Function
 # ------------------------
-def create_embed(title, description_lines, image_url, thumbnail_url, footer_text, footer_icon):
-    description = '\n'.join([f"> {line}" for line in description_lines])
+def create_embed(title, description_lines, image_url, thumbnail_url, footer_text, footer_icon, author_icon):
+    description = '\n'.join(description_lines)
     embed = discord.Embed(
-        title=title,  # Title exakt übernommen
+        title=title,
         description=description,
         color=0x8f1eae
     )
-    embed.set_author(name="Supernova x Welcomer")
+    embed.set_author(name="Supernova x Welcomer", icon_url=author_icon)
     embed.set_image(url=image_url)
     embed.set_thumbnail(url=thumbnail_url)
     embed.set_footer(text=footer_text, icon_url=footer_icon)
     return embed
+
+AUTHOR_ICON = "https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
 
 # ------------------------
 # Events
@@ -70,21 +72,16 @@ async def on_member_join(member):
     channel = bot.get_channel(WELCOME_CHANNEL_ID)
     if channel:
         description_lines = [
-            "🛬 First how was your flight?",
-            "🔎 Here you find your ⁠test",
-            "🥇 At least but not last our ⁠test2",
-            "",
-            f"👤User: {member.mention}",
-            f"⌛ TotalUserCount: {member.guild.member_count}",
-            "🔗 InviteLink: [Supernova Invite Link](https://discord.gg/3mRkJ6UYwx)"
+            f"Hey {member.mention} welcome to **discord.gg/supernova**."
         ]
         embed = create_embed(
-            title="👋 Welcome to Supernova | Hosted by Levin Yilmaz",
+            title="**Welcome to discord.gg/supernova**",
             description_lines=description_lines,
             image_url="https://media.discordapp.net/attachments/1401822345953546284/1418437611193765888/Welcome.png?ex=68ce1e77&is=68ccccf7&hm=ea11922ec548a7438ce45a26eb96988ce281f7e9b57ebd240ffea5d3778f452e&=",
             thumbnail_url="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
             footer_text="© 2022–2024 Superbova. All Rights Reserved.",
-            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
+            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848",
+            author_icon=AUTHOR_ICON
         )
         await channel.send(embed=embed, allowed_mentions=discord.AllowedMentions(users=True))
 
@@ -93,22 +90,16 @@ async def on_member_remove(member):
     channel = bot.get_channel(GOODBYE_CHANNEL_ID)
     if channel:
         description_lines = [
-            "",
-            "🍀 We wish you a good Travel.",
-            "🔎 And we hope you find what you searching for.",
-            "🥇 It was a honour for us to had you on our Server!",
-            "",
-            f"👤User: {member.name}",  # Ping beim Leave technisch nicht möglich
-            f"⌛ TotalUserCount: {member.guild.member_count}",
-            "🔗 InviteLink: [Supernova Invite Link](https://discord.gg/3mRkJ6UYwx)"
+            "Have a good Day from **discord.gg/supernova**."
         ]
         embed = create_embed(
-            title="👋 Thanks for your Visit | Hosted by Levin Yilmaz",
+            title="**Goodbye from discord.gg/supernova**",
             description_lines=description_lines,
             image_url="https://media.discordapp.net/attachments/1401822345953546284/1418437610828988546/Good-Bye.png?ex=68ce1e77&is=68ccccf7&hm=bcf1f0ebb001ceeb793c4ed9b7022973d6d057b6ba34fcd171f3ca587b122499&=",
             thumbnail_url="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
             footer_text="© 2022–2024 Superbova. All Rights Reserved.",
-            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
+            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848",
+            author_icon=AUTHOR_ICON
         )
         await channel.send(embed=embed)
 
