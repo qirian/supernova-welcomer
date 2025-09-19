8f4d65ada2ccff6b8bcc0fd93e2d52c8d6fb1f62
Levin Yilmaz <qirianprivate@hotmail.com>
2025-09-19 05:55:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,28 +25,30 @@ keep_alive()
 # Discord Bot Setup
 # ------------------------
 intents = discord.Intents.default()
-intents.members = True  # für Join/Leave Events
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Feste Channel IDs
+# ------------------------
+# Channels
+# ------------------------
 WELCOME_CHANNEL_ID = 1418440616131428482
 GOODBYE_CHANNEL_ID = 1418441039701610516
 
 # ------------------------
-# Utility Function für Embeds
+# Utility Function
 # ------------------------
-def create_embed(title, description, image_url, thumbnail_url, author_name, author_icon, footer_text):
-    description = '\n'.join([f"> {line}" for line in description.split('\n')])
+def create_embed(title, description_lines, image_url, thumbnail_url, footer_text, footer_icon):
+    description = '\n'.join([f"> {line}" for line in description_lines])
     embed = discord.Embed(
         title=f"# {title}",
         description=description,
         color=0x8f1eae
     )
+    embed.set_author(name="Supernova x Welcomer")
     embed.set_image(url=image_url)
     embed.set_thumbnail(url=thumbnail_url)
-    embed.set_author(name=author_name, icon_url=author_icon)
-    embed.set_footer(text=footer_text)
+    embed.set_footer(text=footer_text, icon_url=footer_icon)
     return embed
 
 # ------------------------
@@ -55,7 +57,6 @@ def create_embed(title, description, image_url, thumbnail_url, author_name, auth
 @bot.event
 async def on_ready():
     print(f"✅ Eingeloggt als {bot.user}")
-
     # Streaming Status setzen
     activity = discord.Streaming(
         name="discord.gg/supernova",
@@ -68,18 +69,21 @@ async def on_ready():
 async def on_member_join(member):
     channel = bot.get_channel(WELCOME_CHANNEL_ID)
     if channel:
+        description_lines = [
+            "🛬 First how was your flight?",
+            "🔎 Here you find your https://discord.com/channels/1401492898293481505/1418437919319785542",
+            "🥇 At least but not last our https://discord.com/channels/1401492898293481505/1418437936709636216",
+            f"👤User: {member.mention}",
+            f"⌛ TotalUserCount: {member.guild.member_count}",
+            "🔗 InviteLink: [Supernova Invite Link.](https://discord.gg/3mRkJ6UYwx)"
+        ]
         embed = create_embed(
-            title="Welcome to Supernova | Hosted by Levin Yilmaz",
-            description=(
-                "We wish you a good Travel.\n"
-                "And we hope you find what you searching for.\n\n"
-                "It was a honour for us to had you on our Server!"
-            ),
-            image_url="https://media.discordapp.net/attachments/1401822345953546284/1418437610828988546/Good-Bye.png?ex=68ce1e77&is=68ccccf7&hm=bcf1f0ebb001ceeb793c4ed9b7022973d6d057b6ba34fcd171f3ca587b122499&=",
+            title="👋Welcome to Supernova | Hosted by Levin Yilmaz",
+            description_lines=description_lines,
+            image_url="https://media.discordapp.net/attachments/1401822345953546284/1418437611193765888/Welcome.png?ex=68ce1e77&is=68ccccf7&hm=ea11922ec548a7438ce45a26eb96988ce281f7e9b57ebd240ffea5d3778f452e&=",
             thumbnail_url="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
-            author_name=member.name,
-            author_icon="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
-            footer_text="© 2022–2024 Superbova. All Rights Reserved."
+            footer_text="© 2022–2024 Superbova. All Rights Reserved.",
+            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
         )
         await channel.send(embed=embed)
 
@@ -87,18 +91,21 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(GOODBYE_CHANNEL_ID)
     if channel:
+        description_lines = [
+            "🍀 We wish you a good Travel.",
+            "🔎 And we hope you find what you searching for.",
+            "🥇 It was a honour for us to had you on our Server!",
+            f"👤User: {member.name}",
+            f"⌛ TotalUserCount: {member.guild.member_count}",
+            "🔗 InviteLink: [Supernova Invite Link.](https://discord.gg/3mRkJ6UYwx)"
+        ]
         embed = create_embed(
-            title="Thanks for your Visit | Hosted by Levin Yilmaz",
-            description=(
-                "We wish you a good Travel.\n"
-                "And we hope you find what you searching for.\n\n"
-                "It was a honour for us to had you on our Server!"
-            ),
+            title="👋Thanks for your Visit | Hosted by Levin Yilmaz",
+            description_lines=description_lines,
             image_url="https://media.discordapp.net/attachments/1401822345953546284/1418437610828988546/Good-Bye.png?ex=68ce1e77&is=68ccccf7&hm=bcf1f0ebb001ceeb793c4ed9b7022973d6d057b6ba34fcd171f3ca587b122499&=",
             thumbnail_url="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
-            author_name=member.name,
-            author_icon="https://cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?size=1024",
-            footer_text="© 2022–2024 Superbova. All Rights Reserved."
+            footer_text="© 2022–2024 Superbova. All Rights Reserved.",
+            footer_icon="https://images-ext-1.discordapp.net/external/ORAM7L-2USvIhk9TKRteJkF9JyLXFa0RNBvrfual4E0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1401488134457524244/067dd861b8a4de1438d12c7bc283d935.webp?width=848&height=848"
         )
         await channel.send(embed=embed)
 
